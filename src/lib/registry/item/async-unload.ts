1a0657ec298076692ddde11ccfa24fcd1efd186f
Euiyeon Kim <ppang327@gmail.com>
2019-09-25 15:42:04 +0900

    refactoring remove dependent urls process

--- src/lib/registry/item/async-unload.ts
+++ src/lib/registry/item/async-unload.ts
@@ -9,10 +9,47 @@ import {
 import { LoaderRegistryItem } from './item'
 import { RegistryItemURL } from './url'
 
-export function asyncRemoveItemUrls(options?: any): Promise<RegistryItemURL[]> {
+export function asyncRemoveDepndentUrls(options?: any): Promise<any>[] | undefined {
+  const dependentUrls = this.getOption('dependentUrls')
+  const isDebugMode = get(options, 'debug', false)
+  if (isNil(dependentUrls)) return
+  const allScriptEls = getElementsByTagName('script')
+  const allLinkEls = getElementsByTagName('link')
+  return castArray(dependentUrls).map(sUrl => {
+    return new Promise((resolve, reject) => {
+      const type = sUrl.indexOf('.css') > 0 ? 'css' : 'js'
+      const bScript = (type === 'js')
+      const attrName = bScript ? 'src' : 'href'
+      const targetList: any = bScript ? allScriptEls : allLinkEls
+      const ndx = findIndex(targetList, (elem: any) => {
+        return includes(elem[attrName], sUrl)
+      })
+      let errMsg
+      if (ndx >= 0) {
+        try {
+          const el = targetList.splice(ndx, 1)[0]
+          el.parentElement.removeChild(el)
+        } catch (err) {
+          errMsg = get(err, 'message', `remove dependent url element error: ${sUrl}`)
+        }
+      } else {
+        errMsg = `not found dependent url element: ${sUrl}`
+      }
+      if (!isNil(errMsg)) {
+        if (isDebugMode) {
+          console.warn(errMsg)
+        }
+        reject({ message: errMsg })
+      }
+      resolve(sUrl)
+    })
+  })
+}
+
+export function asyncRemoveItemUrls(options?: any): Promise<any[]> {
   const self: LoaderRegistryItem = this
   const urls = self.urls
-  const debug = get(options, 'debug', false)
+  const isDebugMode = get(options, 'debug', false)
   let tasks: Promise<any>[] = urls.map(
     (uItem: RegistryItemURL) => {
       return new Promise((resolve, reject) => {
@@ -24,7 +61,7 @@ export function asyncRemoveItemUrls(options?: any): Promise<RegistryItemURL[]> {
           errMsg = `[${url}] failed to remove element in document`
         }
         if (!isNil(errMsg)) {
-          if (debug) {
+          if (isDebugMode) {
             console.warn(errMsg)
           }
           reject({ message: errMsg })
@@ -33,39 +70,8 @@ export function asyncRemoveItemUrls(options?: any): Promise<RegistryItemURL[]> {
       })
     }
   )
-  const dependentUrls = this.getOption('dependentUrls')
-  if (!isNil(dependentUrls)) {
-    const allScriptEls = getElementsByTagName('script')
-    const allLinkEls = getElementsByTagName('link')
-    const subTasks = castArray(dependentUrls).map(sUrl => {
-      return new Promise((resolve, reject) => {
-        const type = sUrl.indexOf('.css') > 0 ? 'css' : 'js'
-        const bScript = (type === 'js')
-        const attrName = bScript ? 'src' : 'href'
-        const targetList: any = bScript ? allScriptEls : allLinkEls
-        const ndx = findIndex(targetList, (elem: any) => {
-          return includes(elem[attrName], sUrl)
-        })
-        let errMsg
-        if (ndx >= 0) {
-          try {
-            const el = targetList.splice(ndx, 1)[0]
-            el.parentElement.removeChild(el)
-          } catch (err) {
-            errMsg = get(err, 'message', `remove dependent url element error: ${sUrl}`)
-          }
-        } else {
-          errMsg = `not found dependent url element: ${sUrl}`
-        }
-        if (!isNil(errMsg)) {
-          if (debug) {
-            console.warn(errMsg)
-          }
-          reject({ message: errMsg })
-        }
-        resolve(sUrl)
-      })
-    })
+  const subTasks = asyncRemoveDepndentUrls.call(this, options)
+  if (!isNil(subTasks)) {
     tasks = concat(tasks, subTasks)
   }
   return Promise.all(tasks)
