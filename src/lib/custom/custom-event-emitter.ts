bc1f235a9f31b052ea1939f3f105d7b87a07827a
임영준 <yjlim@softin.co.kr>
2024-05-10 17:52:18 +0900

    fix: yarn fix

--- src/lib/custom/custom-event-emitter.ts
+++ src/lib/custom/custom-event-emitter.ts
@@ -10,7 +10,7 @@ export class CustomEventEmitter extends EventEmitter {
     return super.emit(event, assignIn({ type: event }, ...args))
   }
   bind(events: string | symbol, listener: (...args: any[]) => void): this {
-    this.setMaxListeners(0); // 11개 이상의 이벤트 리스너 등록 시 경고창 표시 되지 않도록 이벤트 등록 제한 해제
+    this.setMaxListeners(0) // 11개 이상의 이벤트 리스너 등록 시 경고창 표시 되지 않도록 이벤트 등록 제한 해제
     if (isString(events) && events.indexOf(' ') > 0) {
       events.split(' ').forEach(event => {
         this.on(event, listener)
