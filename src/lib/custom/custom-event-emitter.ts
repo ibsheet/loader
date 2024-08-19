73f793b6fd32167aeb93c5c445a288cec1ef6f2b
임영준 <yjlim@softin.co.kr>
2024-08-19 18:02:27 +0900

    fix: EventEmitter 이벤트 리스너 등록 제한 해제 로직 위치 변경

--- src/lib/custom/custom-event-emitter.ts
+++ src/lib/custom/custom-event-emitter.ts
@@ -4,13 +4,13 @@ import { EventEmitter } from 'events'
 export class CustomEventEmitter extends EventEmitter {
   constructor() {
     super()
+    this.setMaxListeners(0) // 11개 이상의 이벤트 리스너 등록 시 경고창 표시 되지 않도록 이벤트 등록 제한 해제
   }
   // @override
   public emit(event: string | symbol, ...args: any[]): boolean {
     return super.emit(event, assignIn({ type: event }, ...args))
   }
   bind(events: string | symbol, listener: (...args: any[]) => void): this {
-    this.setMaxListeners(0) // 11개 이상의 이벤트 리스너 등록 시 경고창 표시 되지 않도록 이벤트 등록 제한 해제
     if (isString(events) && events.indexOf(' ') > 0) {
       events.split(' ').forEach(event => {
         this.on(event, listener)
